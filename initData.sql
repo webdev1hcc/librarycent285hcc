2ed398f85262e51baba2aea28395e6b29927b36c
Vern Takebayashi <takebaya@hawaii.edu>
2011-02-06 19:56:40 -1000

    new file:   initData.sql adds initial records into database

---
+++ initData.sql
@@ -0,0 +1,25 @@
+-- initData.sql - add some initial data
+insert into author_book_view (first_name,last_name,email,title,isbn) values
+  ('Andrew','Hunt','andyhunt@pragprogrammers.com','The Pragmatic Programmer',
+  '999999');
+insert into author_book_view (first_name,last_name,email,title,isbn) values
+  ('Dave','Thomas','davethomas@pragprogrammers.com','The Pragmatic Programmer',
+  '999999');
+insert into author_book_view (first_name,last_name,email,title,isbn) values
+  ('Andrew','Hunt','andyhunt@pragprogrammers.com',
+  'Pragmatic Thinking and Learning','999998');
+insert into author_book_view (first_name,last_name,email,title,isbn) values
+  ('Andrew','Hunt','andyhunt@pragprogrammers.com','Pragmatic Unit Testing',
+  '999997');
+insert into author_book_view (first_name,last_name,email,title,isbn) values
+  ('Dave','Thomas','davethomas@pragprogrammers.com','Pragmatic Unit Testing',
+  '999997');
+insert into author_book_view (first_name,last_name,email,title,isbn) values
+  ('Dave','Thomas','davethomas@pragprogrammers.com',
+  'Agile Web Development with Rails','999996');
+insert into author_book_view (first_name,last_name,email,title,isbn) values
+  ('Sam','Ruby','samruby@pragprogrammers.com',
+  'Agile Web Development with Rails','999996');
+insert into author_book_view (first_name,last_name,email,title,isbn) values
+  ('David Heinemeier','Hansson','dhh@railsrules.com',
+  'Agile Web Development with Rails','999996');
